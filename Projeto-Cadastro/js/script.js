109f7662f74f7ec3caeb2116ce1a6ec59fd049d1
Igor Torres <igortorres234@gmail.com>
2024-10-04 18:19:12 -0300

    montado o templete principal consultoria fake

---
+++ Projeto-Cadastro/js/script.js
@@ -0,0 +1,40 @@
+function MouseHover() {
+    let btn = document.getElementById("bt-financas");
+    btn.innerHTML = "CONFIRA AGORA !";
+}
+
+function MouseOutHover() {
+    let btn = document.getElementById("bt-financas");
+    btn.innerHTML = "SAIBA MAIS";
+}
+
+
+function MouseHover1() {
+    let btn = document.getElementById("bt-marketing");
+    btn.innerHTML = "CONFIRA AGORA !";
+}
+
+function MouseOutHover1() {
+    let btn = document.getElementById("bt-marketing");
+    btn.innerHTML = "SAIBA MAIS";
+}
+
+function MouseHover2() {
+    let btn = document.getElementById("bt-processos");
+    btn.innerHTML = "CONFIRA AGORA !";
+}
+
+function MouseOutHover2() {
+    let btn = document.getElementById("bt-processos");
+    btn.innerHTML = "SAIBA MAIS";
+}
+
+function MouseHover3() {
+    let btn = document.getElementById("bt-pessoas");
+    btn.innerHTML = "CONFIRA AGORA !";
+}
+
+function MouseOutHover3() {
+    let btn = document.getElementById("bt-pessoas");
+    btn.innerHTML = "SAIBA MAIS";
+}
